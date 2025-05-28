5baa92fccb90ec8055959c9a8b065a6fba93980f
Utsav9303 <utsavmodi000@gmail.com>
2025-05-28 15:44:25 +0530

    day2

---
+++ Day-2/MovieManagement/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Model;
+using MovieManagement.Services;
+using MovieManagement.Model;
+using MovieManagement.Services;
+
+namespace Movie_Management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieService _movieService;
+
+        public MoviesController(MovieService movieService)
+        {
+            _movieService = movieService;
+        }
+
+        [HttpGet("GetAllMovies")]
+        public ActionResult<List<Movie>> GetAllMovies()
+        {
+            var movies = _movieService.GetMovies();
+            return movies == null || movies.Count == 0 ? NotFound("No movies found") : Ok(movies);
+        }
+
+        [HttpGet("GetSingleMovie")]
+        public ActionResult<Movie> GetMovie(int id)
+        {
+            var movie = _movieService.GetMovieById(id);
+            return movie == null ? NotFound("Movie not found") : Ok(movie);
+        }
+
+        [HttpPost]
+        public ActionResult AddMovie(Movie movie)
+        {
+            _movieService.AddMovie(movie);
+            return Ok("Movie added successfully");
+        }
+
+        [HttpPut]
+        public ActionResult UpdateMovie(Movie movieToBeUpdated)
+        {
+            var status = _movieService.UpdateMovie(movieToBeUpdated);
+            return status == -1 ? NotFound("Movie not found") : Ok("Movie updated successfully");
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteMovie(int id)
+        {
+            var status = _movieService.DeleteMovie(id);
+            return status == -1 ? NotFound("Movie not found") : Ok("Movie deleted successfully");
+        }
+    }
+}
---
+++ Day-2/MovieManagement/Services/MovieService.cs
@@ -0,0 +1,49 @@
+﻿using MovieManagement.Model;
+
+namespace MovieManagement.Services
+{
+    public class MovieService
+    {
+        private List<Movie> movies;
+
+        public MovieService()
+        {
+            movies = new List<Movie>()
+            {
+                new Movie() { Id = 1, Title = "Inception", Director = "Christopher Nolan", Genre = "Sci-Fi", ReleaseYear = 2010 },
+                new Movie() { Id = 2, Title = "Interstellar", Director = "Christopher Nolan", Genre = "Sci-Fi", ReleaseYear = 2014 }
+            };
+        }
+
+        public List<Movie> GetMovies() => movies;
+
+        public Movie GetMovieById(int id) => movies.FirstOrDefault(m => m.Id == id);
+
+        public void AddMovie(Movie movie)
+        {
+            movie.Id = movies.Count + 1;
+            movies.Add(movie);
+        }
+
+        public int UpdateMovie(Movie updatedMovie)
+        {
+            var movie = GetMovieById(updatedMovie.Id);
+            if (movie == null) return -1;
+
+            movie.Title = updatedMovie.Title;
+            movie.Director = updatedMovie.Director;
+            movie.Genre = updatedMovie.Genre;
+            movie.ReleaseYear = updatedMovie.ReleaseYear;
+            return 1;
+        }
+
+        public int DeleteMovie(int id)
+        {
+            var movie = GetMovieById(id);
+            if (movie == null) return -1;
+
+            movies.Remove(movie);
+            return 1;
+        }
+    }
+}
